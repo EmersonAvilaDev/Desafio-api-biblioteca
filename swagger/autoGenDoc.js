ecd3939bc34cf5bfc73ddee91c321be3753315e6
EMERSON AVILA <106774143+EmersonAvilaDev@users.noreply.github.com>
2023-09-16 02:07:56 -0300

    instalação de bibliotecas npm e inicialização do projeto com express.js

---
+++ swagger/autoGenDoc.js
@@ -0,0 +1,31 @@
+const mongooseToSwagger = require("mongoose-to-swagger");
+const swaggerAutogen = require("swagger-autogen")({
+  openapi: "3.0.0",
+  language: "pt-BR",
+});
+
+const outputFile = "./swagger_output.json";
+const endpointFiles = ["../index.js"];
+
+let doc = {
+  info: {
+    version: "1.0.0",
+    title: "API da biblioteca",
+    description: "Documentação da API da biblioteca",
+  },
+  servers: [
+    {
+      url: "http://localhost:4000/",
+      description: "Servidor localhost",
+    },
+  ],
+  consumes: ["application/json"],
+  produces: ["application/json"],
+};
+
+swaggerAutogen(outputFile, endpointFiles, doc).then(() => {
+  console.log("Documentação do Swagger gerada encontra-se no arquivo em: " + outputFile);
+  if (process.env.NODE_ENV !== "production") {
+    require("../index.js");
+  }
+});
