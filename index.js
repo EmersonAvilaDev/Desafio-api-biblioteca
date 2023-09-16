691a63be4abc59cc9bae4e95059276eae148d3b9
EMERSON AVILA <106774143+EmersonAvilaDev@users.noreply.github.com>
2023-09-16 03:07:32 -0300

    Criação da documentação do README.md  e  adicionei os arquivos principais para dentro da src

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerOptions = {
   customCssUrl: "./swagger-ui.css",
 };
-const swaggerFile = require("./swagger/swagger_output.json");
+const routes = require("./src/routes");
 
-const usersRouter = require("./routes/users");
 
 const app = express();
 require("dotenv").config();
+
 
 app.use(cors());
 app.use(logger("dev"));
@@ -21,18 +21,27 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  /* #swagger.ignore = true */ res.redirect("/api-doc");
-});
-app.use(
-  "/api-doc",
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerFile, swaggerOptions)
-);
-app.use("/users", usersRouter);
+
+if (process.env.NODE_ENV !== "test") {
+  const swaggerFile = require("./swagger/swagger_output.json");
+
+  app.get("/", (req, res) => {
+    /* #swagger.ignore = true */ res.redirect("/api-doc");
+  });
+  app.use(
+    "/api-doc",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerFile, swaggerOptions)
+  );
+}
+
+
+routes(app);
+
 
 if (process.env.NODE_ENV !== "test") {
   const PORT = process.env.PORT || 4000;
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
+
 module.exports = app;
